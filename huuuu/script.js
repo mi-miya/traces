d7c3a608ebeadef9605462fa85806455b3fafd43
mimi_2046 <m.miyahara2046@gmail.com>
2020-01-29 13:42:31 +0900

    huuuu トレース途中

--- huuuu/script.js
+++ huuuu/script.js
@@ -1,17 +1,17 @@
 let oldPos = 0;
 let timeoutId = 0;
 let scrolls = [];
-let minShakeAngle = 0.3; // 最小振幅角
+let minShakeAngle = 0.3; // 最小振れ角
 let attenuation = 0.8; // 振幅減衰率
 let defaultAngle = 45; // 基準振れ角
 
 window.onload = () => {
   oldPos = window.scrollY;
   let shakes = Array.from(document.getElementsByClassName('shakes'));
-  shakes.forEach((shake, index) => {
+  shakes.forEach((shake) => {
     scrolls.push(new Scroll(shake));
   });
-  
+
   window.addEventListener( "scroll", () => {
     clearTimeout( timeoutId );
     timeoutId = setTimeout( function () {
@@ -22,7 +22,7 @@ window.onload = () => {
         scroll.shakeSet(angle);
       });
       oldPos = window.scrollY;
-    }, 60 );
+    }, 15);
   });
 }
 
@@ -36,10 +36,9 @@ class Scroll {
     this.angleRate = Math.random() + 0.5;
     this.animationInterval = 200 * Math.random() + 500;
     el.style.transition = `transform ${this.animationInterval}ms ease-in-out`;
+    this.shakeSet(10);
   }
 
-  // TODO: ボックスの挙動がおかしい
-  // どこがおかしいか分からん
   shakeSet(angle) {
     angle *= this.angleRate;
     if(this.maxAngle < angle) {
