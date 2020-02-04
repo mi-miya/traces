00d8f63605b37cbd2c11a55c11d306b4e53c2de4
mimi_2046 <m.miyahara2046@gmail.com>
2020-02-04 09:51:49 +0900

    funplex スクロールボタン追加

---
+++ funplex/javascripts/script.js
@@ -0,0 +1,43 @@
+let selected;
+let position = {
+  top: null,
+  about: null,
+  service: null,
+  recruit: null
+}
+
+window.onload = () => {
+  let index = 0;
+  let svg_area = document.getElementById('svg_area');
+  let svg_bg = document.getElementById('svg_bg');
+  let r = Raphael("svg_area", "100%", "100%");
+  position['top'] = document.getElementById('top');
+  position['about'] = document.getElementById('about');
+  position['service'] = document.getElementById('service');
+  position['recruit'] = document.getElementById('recruit');
+  selected = document.getElementsByClassName('selected')[0];
+  r.setViewBox(0, 0, svg_area.clientWidth / 16, svg_area.clientHeight / 16, false);
+  let letter = r.path("M0,0L0,0z");
+  letter.animate({path: game[index]});
+  svg_bg.innerHTML= `<img style="animation: bgZoom 4.5s ease-out;" src="images/${index}.jpg">`;
+  index++;
+  setInterval(() => {
+    letter.animate({path: game[index]}, 250);
+    svg_bg.innerHTML= `<img style="animation: bgZoom 4.5s ease-out;" src="images/${index}.jpg">`;
+    index++;
+    if(index >= 4) {
+      index = 0;
+    }
+  }, 4500);
+}
+
+function clickLinks(pos) {
+  selected.classList.remove('selected');
+  selected = event.target;
+  selected.classList.add('selected');
+  window.scrollBy({
+    left: 0, 
+    top: position[pos].getBoundingClientRect().top - 95,
+    behavior: 'smooth'
+  });
+}
